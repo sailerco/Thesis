1beff26623b93716c3d89e7c6c90aa0e75a37061
Corinna <corinna.jasmin.sailer@hs-furtwangen.de>
2024-08-28 14:03:48 +0200

    feat: refactored

--- MetricsGenerator.py
+++ MetricsGenerator.py
@@ -8,36 +8,73 @@ from tabulate import tabulate
 
 
 def read_file(path):
-    #    csv = pd.read_csv(path, header=None, index_col=None, usecols=[1,2], skiprows=[0])
+    """
+    Reads a CSV file and returns the data as a DataFrame.
+    Returns:
+        pd.DataFrame: The content of the CSV file as a DataFrame, without the header column and row.
+    """
     csv = pd.read_csv(path, header=None, index_col=None, skiprows=[0]).drop(columns=0)
+    # when using only specific columns, use the following: csv = pd.read_csv(path, header=None, index_col=None, usecols=[1,2], skiprows=[0])
     return csv
 
 
 def calculate_label_density(predictions):
+    """
+    Calculates the label density of predictions. The label density is the mean number of labels assigned to the samples.
+
+    Returns:
+        float: The mean label density.
+    """
     label_cardinality = np.sum(predictions, axis=1)
     label_density = np.mean(label_cardinality)
     return label_density
 
 
 def set_truth(name, truth_dir):
+    """set the truth file path"""
     return f'truth{name}.csv' if truth_dir else 'truth.csv'
 
 
 class MetricsGenerator:
-    header = ['Threshold', 'Label Density', 'Subset Accuracy', 'Recall', 'F1 Score', 'F-Beta Score', 'Hamming Loss', "ROC AUC", "Jaccard Loss"]
+    """
+    A class to generate and compare classification metrics for different models.
+
+    Attributes:
+        header (list): Column names for the metrics table.
+        header (list): Column names for the metrics table.
+        avg (str): The averaging method for calculating metrics (default is 'macro').
+        truth (str): The file name of the ground truth data.
+        thresholds (list): List of thresholds to apply to the predictions.
+        ROOT_DIR (str): The root directory of the project.
+        BART_METRICS (str): The file path to the original BART metrics CSV.
+        DEBERTA_METRICS (str): The file path to the original DEBERTA metrics CSV.
+        compareBart (bool): A flag to determine which model's metrics to compare.
+    """
+
+    header = ['Threshold', 'Label Density', 'Subset Accuracy', 'Recall', 'F1 Score', 'F-Beta Score', 'Hamming Loss', "Jaccard Index"]
+
     avg = 'macro'
     truth = None
 
     thresholds = [1.0, 0.95, 0.9, 0.8, 0.5]
-    file_dir = os.getcwd()
-    file_dir = os.path.dirname(file_dir)
-    if file_dir.endswith("Synth"):
-        file_dir = os.path.dirname(file_dir)
-    BART_METRICS = os.path.join(file_dir, "Classification_Synth", "GT", "metrics", "bart_metrics.csv")
-    DEBERTA_METRICS = os.path.join(file_dir, "Classification_Synth", "GT", "metrics", "deberta_metrics.csv")
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
+    BART_METRICS = os.path.join(ROOT_DIR, "Classification_Synth", "GroundTruth", "metrics", "bart_metrics.csv")
+    DEBERTA_METRICS = os.path.join(ROOT_DIR, "Classification_Synth", "GroundTruth", "metrics", "deberta_metrics.csv")
     compareBart = None
 
     def __init__(self, name, file_dir, end_dir, truth_dir, bart, deberta):
+        """
+        Initializes the MetricsGenerator with necessary parameters.
+
+        Args:
+            name (str): The (path) name of the model or variant.
+            file_dir (str): Directory containing the ground truth files.
+            end_dir (str): Directory containing the prediction files.
+            truth_dir (bool): Flag indicating whether the truth dir is default or differs
+            bart (bool): Flag indicating whether to compute BART metrics.
+            deberta (bool): Flag indicating whether to compute DEBERTA metrics.
+        """
         self.bart = bart
         self.deberta = deberta
         self.truth_dir = truth_dir
@@ -46,23 +83,42 @@ class MetricsGenerator:
         self.end_dir = end_dir
 
     def get_metrics_values(self, ground_truth, prediction, threshold):
+        """
+        Computes various metrics between the ground truth and predictions. The metrics include accuracy, recall,
+        F1 score, F-Beta score, hamming loss, and jaccard index.
+
+        Args:
+            ground_truth (pd.DataFrame): The ground truth data.
+            prediction (pd.DataFrame): The predicted data.
+            threshold (float): The threshold applied to the predictions.
+
+        Returns:
+            list: A list of calculated metric values, including the threshold used.
+        """
         ground_truth = ground_truth.to_numpy()
         prediction = prediction.to_numpy()
         metrics_value = [
             round(threshold, 4),
             round(calculate_label_density(prediction.T), 2),
-            round(metrics.accuracy_score(ground_truth.T, prediction.T), 4),  # -> hohe Werte besser
+            round(metrics.accuracy_score(ground_truth.T, prediction.T), 4),
             round(metrics.recall_score(ground_truth, prediction, average=self.avg), 4),
-            round(metrics.f1_score(ground_truth, prediction, average=self.avg), 4),  # -> hohe Werte besser
+            round(metrics.f1_score(ground_truth, prediction, average=self.avg), 4),
             round(metrics.fbeta_score(ground_truth, prediction, beta=2, average=self.avg), 4),
-            round(metrics.hamming_loss(ground_truth, prediction), 4),  # -> niedrige Werte besser
-            round(metrics.roc_auc_score(ground_truth, prediction), 4),
+            round(metrics.hamming_loss(ground_truth, prediction), 4),
             round(metrics.jaccard_score(ground_truth, prediction, average=self.avg), 4)
         ]
 
         return metrics_value
 
-    def compare(self, df):
+    def compare_with_original_metrics(self, df):
+        """
+        Compares the calculated metrics with existing BART or DEBERTA metrics
+        (default: metrics of first classification without fine-tuned models).
+        This method reads the previous metrics from a CSV file and calculates the differences with the current metrics.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the current metrics.
+        """
         if self.compareBart:
             old = pd.read_csv(self.BART_METRICS)
         else:
@@ -72,7 +128,11 @@ class MetricsGenerator:
         print(tabulate(differences, headers=self.header, showindex=False))
 
     def get_files(self, file):
-        # read the truth and prediction
+        """
+        Reads the ground truth and prediction files.
+        This method loads the ground truth and prediction data from the respective directories,
+        fills missing values in the ground truth data, and returns both datasets.
+        """
         ground_truth = read_file(os.path.join(self.file_dir, self.truth))
         prediction = read_file(os.path.join(self.end_dir, f'{file}{self.name}.csv'))
 
@@ -82,18 +142,25 @@ class MetricsGenerator:
         return ground_truth, prediction
 
     def get_metrics(self, file):
+        """
+        Calculates metrics for a given prediction file at various thresholds.
+        This method calculates metrics for each threshold and compares them with the original metrics.
+
+        Args:
+            file (str): The name of the prediction file (e.g., 'bart' or 'deberta').
+        """
         ground_truth, prediction = self.get_files(file)
-        # set threshold and create metrics
+
         table = []
         for threshold in self.thresholds:
             threshold_prediction = prediction.applymap(lambda x: 1 if x >= threshold else 0)
             values = self.get_metrics_values(ground_truth, threshold_prediction, threshold)
             table.append(values)
+
         print(tabulate(table, headers=self.header))
 
-        # compare with original metric
-        df = pd.DataFrame(table, columns=self.header)
-        self.compare(df)
+        resulting_metrics = pd.DataFrame(table, columns=self.header)
+        self.compare_with_original_metrics(resulting_metrics)
 
     def main(self):
         print(f"---{self.name}---")
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+#### Restarting/Connecting to MySQL
+```"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld" --console```
 
+Properties
+- localhost 3306
+- user: root
+- password: 1234
+- database: mysql
 
-#### Postgres SQL
+#### Postgres SQL 
 ```docker run --name some-postgres -e POST-GRES_PASSWORD=mysecretpassword -p 5432:5432 -v D:\postgres-data:/var/lib/postgresql/data -d postgres```  
 1. Create skill tabel  
 `create table skills (
@@ -28,7 +35,114 @@ proficiency_lvl proficiency
 )`  
 5. Run script `DB/dbGenerate.py`
 
-Run Pipeline
+# Run Pipeline
 1. open the Thesis folder in a terminal (not the IDE terminal, if it doesn't support cursors)
 2. activate venv `venv\Scripts\activate.bat`
-3. run script `Pipeline\pipeline.py`
+3. run script `python Pipeline\pipeline.py`
+
+# Directory Structure
+The project is organized as follows:
+### Zero-Shot Classification with the synthesized data
+````
+.
+├── Classification_Synth/  
+│   ├── ClassifierOutput/  
+│   ├── GroundTruth/  
+│   ├── generate.py  
+│   ├── truth.py 
+│   ├── userStories.csv  
+│   └── ZeroShotClassificator.py
+````
+The `Classification_Synth` Package consists of multiple functions. The center of attention is the
+`ZeroShotClassificator.py`. The script uses the synthesized user stories (`userStories.csv`) and the skills of the employee dataset
+and does a zero-shot classification. 
+In the `ClassifierOutput` the resulting txt and csv files are saved. 
+The `Ground Truth` contains resulting metrics (for bart and deberta) in a csv file, which are used for comparison in later stages.
+
+### Zero-Shot Classification with data from the TAWOS database
+````
+├── Classification_TAWOS/  
+│   ├── assets/...  
+│   ├── ClassifierOutput/...  
+│   ├── final_assets/...  
+│   ├── GroundTruth/...  
+│   ├── csvConv.py  
+│   ├── ExtractAllUserStories.py  
+│   ├── ReduceAndGroup.py  
+│   ├── ZeroShotClassificatorLooped.py  
+│   └── ZeroShotClassificatorTest.py  
+````
+For this classification data extracted from the TAWOS database through `ExtractAllUserStories.py`. 
+It is essential to reduce, group or both through the `ReduceAndGroup.py` script. 
+In the assets folder is a `cleaned.csv` (which contains the components reduced by hand) 
+as well as a `grouped_comps.yml` file which helps in the grouping process in the `ReduceAndGroup.py` script
+The `final_asstets` folder contains the user stories and the components after reducing/grouping which are used for the 
+zero-shot classification as well as the output_csv and output_txts.
+
+### Zero-Shot Classification with data from the TAWOS database and the skills from the employee database before and after fine-tuning
+````
+├── Classification_TAWOS_Synth/  
+│   ├── assets  
+│   ├── PostFineTuning  
+│   ├── PreFineTuning  
+│   ├── compare_two_prediction_sets.py
+│   ├── display_entailments.py
+│   ├── generate_metrics_table.py
+│   ├── plots_histogram.py  
+│   ├── select_random_user_stories.py
+│   ├── sort_data.py  
+│   ├── truth.csv
+│   └── Zero_Shot_Classification.py
+````
+The assets folder contains a csv file which contains 25 random user stories (origning from TAWOS). These are randomly 
+selected through the `select_random_user_stories.py` script.  
+The `PostFineTuning` and `PreFineTuning` Folders contain the resulting predictions of the `Zero_Shot_Classification.py` script.
+Other than that, the folder contains different scripts for generating metrics or evaluations.
+
+### Development of the employee database
+````
+├── DB/
+│   ├── datasets
+│   ├── GenerationOfStories
+│   ├── output
+│   ├── addDescription.py
+│   ├── csvGenerator.py
+│   ├── dbGenerate.py
+│   └── generateSkillTable.py
+````
+The DB folder represents the code, which was used to create the employee database.
+Therefore, there were a csv files for:
+- random names, 
+- a batch of [roles and role descriptions](https://www.kaggle.com/datasets/anujrajawat/itjobs-and-skills), 
+- a portion of the [roles](https://www.kaggle.com/datasets/anujrajawat/itjobs-and-skills) and which were matches with [skills](https://www.kaggle.com/datasets/zamamahmed211/skills) -> matched manually
+- a batch of [skills](https://www.kaggle.com/datasets/zamamahmed211/skills)
+
+The `dbGenerate.py` script generates the data for the employee database. The setup of the database, 
+if no dump is available is described in [here](#postgres-sql-)
+
+### Fine-Tuning
+````
+├── FineTuning/
+│   ├── FinalRuns
+│   ├── FinalRuns_PreTrained
+│   ├── FinalRuns_PreTrained_bart
+│   ├── FinalRuns_PreTrained_deberta
+│   ├── output_csv
+│   ├── output_txt
+│   ├── finetuning-Trainer.ipynb
+│   ├── metrics.py
+│   └── truth.csv
+````
+The Fine-Tuning contains the resulting models from the Fine-Tuning runs as well as the predictions outputs of a zero shot classification with the new models.
+In the `fine_tuning.ipynb` is the preparation and the actual fine-tuning located as well as a zero-shot classification
+
+### Pipeline
+````
+├── Pipeline
+    └── pipeline.py
+````
+The `pipeline` script allows users to select a model, input a user story, 
+and perform zero-shot classification on a predefined set of skills. Based on the positively classified (entailment) skills, 
+the script can filter individuals who are proficient in those specific skills.
+
+![pipeline_flowchart.png](Pipeline/pipeline_flowchart.png)
